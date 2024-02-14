c045fddd5c462462133fa09308c0693e54d30c05
Andrey Zolotin <azolotin@anaconda.com>
2024-02-14 16:42:33 +0100

    feat: add german translators code

--- german-translators/main.py
+++ german-translators/main.py
@@ -6,6 +6,10 @@ import pandas as pd
 from multiprocessing import Pool
 
 
+all_translators_list = []
+result = []
+persons = []
+
 def get_translators(state_code, display_start, display_length):
     data['Bundesland'] = state_code
     data['iDisplayStart'] = display_start
@@ -32,30 +36,16 @@ def get_state_translators(state_code):
     print(f"Completed: {state_code} with {len(state_translators)} translators")
     return state_translators
 
-
-all_translators_list = []
-result = []
-persons = []
-
-if __name__ == "__main__":
-    with Pool(16) as pool:
-        responses = pool.map(get_state_translators, states)
-
-    for response in responses:
-        all_translators_list.extend(response)
-
-    print(len(all_translators_list))
-# for state_code in states:
-#     all_translators_list.extend(get_state_translators(state_code))
-
-# dataframe = pd.DataFrame(all_translators_list)
-# print(f"Total: {len(dataframe)}")
-#
-
 def get_person_details(person):
     person_id = person['PersonId']
     print(f"Processing: {person_id}")
     return scrap_data_by_id(person_id)
+
+if __name__ == "__main__":
+    with Pool(16) as pool:
+        responses = pool.map(get_state_translators, states)
+    for response in responses:
+        all_translators_list.extend(response)
 
 if __name__ == "__main__":
     with Pool(16) as pool:
